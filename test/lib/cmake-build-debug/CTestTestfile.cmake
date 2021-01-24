e94874948868307eea8f51708e464369ca075a5c
Michal Kyjovsky <michalkyjovsky@hotmail.com>
2021-01-24 22:36:59 +0100

    Adding GTEST

---
+++ test/lib/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/micha/MFF_UK/NPRG041/course_project/data-integrity-checker/test/lib
+# Build directory: D:/micha/MFF_UK/NPRG041/course_project/data-integrity-checker/test/lib/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ test/lib/cmake-build-debug/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/micha/MFF_UK/NPRG041/course_project/data-integrity-checker/test/lib/googletest
+# Build directory: D:/micha/MFF_UK/NPRG041/course_project/data-integrity-checker/test/lib/cmake-build-debug/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
